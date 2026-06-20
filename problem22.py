de1c60da8ba517b2f00d35d27b15e38dee89b364
d0ofus0 <jainsamyak.2018@gmail.com>
2026-06-20 14:03:28 -0400

    Problem 22

---
+++ problem22.py
@@ -0,0 +1,18 @@
+import math
+
+with open("0022_names.txt", "r", encoding="utf-8") as file:
+    names_1 = file.read()
+    
+names = names_1.split(",")
+names = [name.strip("\"").lower() for name in names]
+
+names = sorted(names)
+
+s = 0
+
+for i in range(len(names)):
+    score_list = [ord(c) - 96 for c in names[i]]
+    score = sum(score_list) * (i + 1)
+    s += score
+
+print(s)
