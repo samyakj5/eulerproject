42c77f9bb750ebf235ac2d63f1ae376656994852
d0ofus0 <jainsamyak.2018@gmail.com>
2026-06-28 10:09:44 -0400

    Problem 112

---
+++ problem112.py
@@ -0,0 +1,20 @@
+bouncy_nums = 0
+total_nums = 0
+
+curr = 1
+
+while True:
+    if total_nums:
+        if bouncy_nums / total_nums == 0.99:
+            break
+    if sorted(str(curr)) == list(str(curr)):
+        total_nums += 1
+    elif sorted(str(curr))[::-1] == list(str(curr)):
+        total_nums += 1
+    else:
+        bouncy_nums += 1
+        total_nums += 1
+
+    curr += 1
+
+print(curr - 1)
